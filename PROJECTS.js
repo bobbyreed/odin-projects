4a7bcfce39f2bde9e5a995587df2e5baaa46da56
bobby <37113385+bobbyreed@users.noreply.github.com>
2025-12-13 19:23:33 -0600

    Inital bones

---
+++ PROJECTS.js
@@ -0,0 +1,22 @@
+export const PROJECTS = {
+    // Use the template below to add projects
+    // "example-project": {
+    //     title: "Example Project",
+    //     githubURL: "github.com/bobbyreed",
+    //     description: "A high-stakes vertical falling game where Starsky the Ram descends from the stars to Earth through 10 increasingly challenging atmospheric stages. Navigate through space debris, weather phenomena, and urban obstacles while threading the needle for near-misses and maximum points.",
+    //     liveURL: "https://jump.abobbyreed.com"
+    // },
+    "rps": {
+        title: "Rock, paper, scissors (Lizard, Spock)",
+        githubURL: "https://github.com/bobbyreed/odin-rock-paper-scissors",
+        description: "Two versions of the rock, paper, scissors game. IN the second version I implemented Lizard and Spock as well.",
+        liveURL: "https://bobbyreed.github.io/odin-rock-paper-scissors/"
+    },
+    "etch-a-sketch": {
+        title: "Etch-A-Sketch",
+        githubURL: "https://github.com/bobbyreed/odin-etch-a-sketch",
+        description: "Two versions of the classic kids toy. In the second version the awful knobs are implemented.",
+        liveURL: "https://bobbyreed.github.io/odin-etch-a-sketch/"
+    }
+};
+
---
+++ script.js
@@ -0,0 +1,8 @@
+import { PROJECTS } from "./PROJECTS";
+
+const container = document.getElementById("container");
+
+PROJECTS.forEach(project => {
+    
+});
+
